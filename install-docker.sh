53c6eaf98c5e56b8df1ac96424bfa4d5fd2f5ab7
ranjith2007 <ranjithkumar0799@gmail.com>
2025-11-05 18:50:39 +0530

    docker

---
+++ install-docker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#check whether root user is there or not
+R="\e[31m"
+N="\e[0m"
+
+dnf -y install dnf-plugins-core
+dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+systemctl start docker
+systemctl enable docker
+usermod -aG docker ec2-user
+echo -e "$R Logout and Login again $N"
